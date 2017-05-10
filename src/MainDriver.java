16a7ce9a03828d3f03d0cf45c4a1378063ebef9a
Michael Carbonari <mikecarbonari1@gmail.com>
2017-05-10 12:42:23 -0400

    Update MainDriver.java

--- src/MainDriver.java
+++ src/MainDriver.java
@@ -12,9 +12,7 @@ public class MainDriver {
 		jf.setResizable(false);// user able to resize window? nope.
 		jf.setSize(500, 500);// 500 px X 500px all they get
 		jf.setVisible(true);// why not invisible
-		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);// close when you
-															// press red 'x' at
-															// top right
+		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);// close when you press red 'x' at top right
 		jf.add(ab);// add address book to jframe
 	}
 
